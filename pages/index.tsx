2506cc660c3cedfbb03fcec2b3520f35a6e87c84
simonerom <mail@sromano.com>
2023-01-03 23:26:03 +0100

    First draft of UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { Button, Heading, Link, VStack } from "@chakra-ui/react"
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <VStack height="100vh"  alignItems="center" justifyContent="center">
+    <Heading>Welcome to Get Proof Of Location!</Heading>
+    <p>Get Proof Of Location is a service that allows you to prove your location at a specific time.</p>
+    <p>It is a service that is built on top of the IoTeX <a href="https://w3bstream.com/">W3bstream</a> platform.</p>
+    <Button><Link href="/app"> Open App</Link></Button>
+  </VStack>
 )
 
 export default IndexPage
