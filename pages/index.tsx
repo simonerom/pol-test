55b0b274a311ea4b1d64d9ba1f95e5adbef73446
simonerom <mail@sromano.com>
2023-01-06 13:44:03 +0100

    Added open in ioPay btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ const IndexPage = () => (
     <p>Get Proof Of Location is a service that allows you to prove your location at a specific time.</p>
     <p>It is a service that is built on top of the IoTeX <a href="https://w3bstream.com/">W3bstream</a> platform.</p>
     <Link href="/app"><Button>Open App</Button></Link>
+    <Link href="iopay://io.iotex.iopay/open?action=web&url=pol-test.vercel.app">
+      <Button colorScheme={"pink"}>Open in ioPay</Button>
+    </Link>
   </VStack>
 )
 
